b1d12c232aea871715b08a3c962eb04c6d45f336
jesusdavidrinconrivera <jesusdavidrinconrivera@gmail.com>
2023-04-18 09:31:01 -0500

    Ventana Creada

--- ConfigVentana.java
+++ ConfigVentana.java
@@ -1,5 +1,17 @@
 package CalcPendiente;
 
-public class ConfigVentana {
-    
+import java.awt.*;
+import javax.swing.*;
+
+public class ConfigVentana extends JFrame {
+    Dimension screenSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().getSize();
+
+    public ConfigVentana() {
+        setVisible(true);
+        setSize(screenSize);
+        setLocationRelativeTo(null);
+        setTitle("Calculadora de Pendiente");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 package CalcPendiente;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        ConfigVentana Ventana = new ConfigVentana();
+    }
 }

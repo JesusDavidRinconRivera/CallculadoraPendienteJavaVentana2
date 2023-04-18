80957fa4b258453df23d49ab44f16d2f08f9ceb9
jesusdavidrinconrivera <jesusdavidrinconrivera@gmail.com>
2023-04-18 09:57:00 -0500

    Panel No se ve

--- ConfigPanel.java
+++ ConfigPanel.java
@@ -1,5 +1,13 @@
 package CalcPendiente;
+import javax.swing.*;
+import java.awt.*;
 
-public class ConfigPanel {
-    
+public class ConfigPanel extends JFrame {
+    private JPanel panelVerde;
+    public ConfigPanel() {
+        JPanel panelVerde = new JPanel();
+        this.getContentPane().add(panelVerde);
+        panelVerde.setBackground(Color.GREEN);
+    }
+
 }
--- ConfigVentana.java
+++ ConfigVentana.java
@@ -4,14 +4,22 @@ import java.awt.*;
 import javax.swing.*;
 
 public class ConfigVentana extends JFrame {
-    Dimension screenSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().getSize();
-
+    Dimension Tamaño = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().getSize();
+    
     public ConfigVentana() {
         setVisible(true);
-        setSize(screenSize);
+        setSize(Tamaño);
         setLocationRelativeTo(null);
         setTitle("Calculadora de Pendiente");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ConfigPanel Paneles = new ConfigPanel(); 
     }
 
-}
+    private void Componentes(){
+        
+            JPanel panelVerde = new JPanel();
+            this.getContentPane().add(panelVerde);
+            panelVerde.setBackground(Color.GREEN);
+        }
+    }
+

7c249a16ef1eeeffce24bf3c7ddb7a6029ecd811
jesusdavidrinconrivera <jesusdavidrinconrivera@gmail.com>
2023-04-18 10:41:13 -0500

    Paneles en la misma ventana

--- ConfigPanel.java
+++ ConfigPanel.java
@@ -1,13 +1,24 @@
 package CalcPendiente;
 import javax.swing.*;
 import java.awt.*;
-
+//Clase que crea los paneles con nombres de colores 
 public class ConfigPanel extends JFrame {
     private JPanel panelVerde;
     public ConfigPanel() {
-        JPanel panelVerde = new JPanel();
-        this.getContentPane().add(panelVerde);
+        panelVerde = new JPanel(new GridBagLayout());
+        panelAzul = new JPanel(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+
         panelVerde.setBackground(Color.GREEN);
+        panelAzul.setBackground(Color.BLUE);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        this.getContentPane().add(panelVerde, gbc);
+
+        gbc.gridx = 1;
+        this.getContentPane().add(panelAzul, gbc);
     }
 
 }
--- ConfigVentana.java
+++ ConfigVentana.java
@@ -4,22 +4,51 @@ import java.awt.*;
 import javax.swing.*;
 
 public class ConfigVentana extends JFrame {
+    private JPanel panelGeneral,panelVerde,panelAzul;
     Dimension Tamaño = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().getSize();
-    
+
     public ConfigVentana() {
         setVisible(true);
         setSize(Tamaño);
         setLocationRelativeTo(null);
         setTitle("Calculadora de Pendiente");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ConfigPanel Paneles = new ConfigPanel(); 
+       // panelVerde = new ConfigPanel();
+        Paneles();
     }
 
-    private void Componentes(){
-        
-            JPanel panelVerde = new JPanel();
-            this.getContentPane().add(panelVerde);
-            panelVerde.setBackground(Color.GREEN);
-        }
+    private void Paneles(){
+        panelGeneral = new JPanel(new GridBagLayout());
+        panelVerde = new JPanel();
+        panelAzul = new JPanel();
+    
+        panelVerde.setBackground(Color.GREEN);
+        panelAzul.setBackground(Color.BLUE);
+    
+        JLabel Calcular = new JLabel("Calcular");
+        panelAzul.add(Calcular);
+    
+        // definir las restricciones de panelVerde
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridheight = 1;
+        gbc.weightx = 0.2;
+        gbc.weighty = 0.8;
+        panelGeneral.add(panelVerde, gbc);
+    
+        // definir las restricciones de panelAzul
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1;
+        gbc.weighty = 0;
+        panelGeneral.add(panelAzul, gbc);
+    
+        this.getContentPane().add(panelGeneral);
     }
-
+    
+    
+}
